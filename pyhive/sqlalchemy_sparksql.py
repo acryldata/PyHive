c804b0646746e67aac41cc28eea6b8679169d789
Piotr Skrydalewicz <pi.skrydalewicz.ext@adevinta.com>
2022-03-12 02:16:05 +0100

    Refactored get_table_names

--- pyhive/sqlalchemy_sparksql.py
+++ pyhive/sqlalchemy_sparksql.py
@@ -46,22 +46,17 @@ class SparkSqlDialect(sqlalchemy_hive.HiveDialect):
     #         return rows
 
     def get_table_names(self, connection, schema=None, **kw):
+        # we SHOW TABLES will show tables and views, SHOW VIEWS only views, if it is needed we could potentially
+        # subtract set of views from set of tables to get a proper list of only tables
+        # since hive dialect implementation does not support views extraction get_view_names need to be reimplemented
+        # too
         query = 'SHOW TABLES'
         if schema:
             query += ' IN ' + self.identifier_preparer.quote_identifier(schema)
-        return list(row[1] for row in filter(
-            lambda x: not x[-1],
-            [row for row in connection.execute(query)]
-        ))
-
-    def get_view_names(self, connection, schema=None, **kw):
-        query = 'SHOW TABLES'
-        if schema:
-            query += ' IN ' + self.identifier_preparer.quote_identifier(schema)
-        return list(row[1] for row in filter(
-            lambda x: x[-1],
-            [row for row in connection.execute(query)]
-        ))
+        # returns tuples ('database', 'tableName', 'isTemporary')
+        result = connection.execute(query)
+        a = [row[1] for row in result if not row[-1]]
+        return a
 
     def has_table(self, connection, table_name, schema=None):
         try:
