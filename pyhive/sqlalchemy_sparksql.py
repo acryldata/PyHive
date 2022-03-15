2db8268284377367fe63f80ccc58bb743866aa88
Piotr Skrydalewicz <pi.skrydalewicz.ext@adevinta.com>
2022-03-15 20:07:07 +0100

    Improvements to the sparksql implementation

--- pyhive/sqlalchemy_sparksql.py
+++ pyhive/sqlalchemy_sparksql.py
@@ -1,3 +1,5 @@
+import re
+
 from . import sqlalchemy_hive
 
 
@@ -11,39 +13,27 @@ class SparkSqlDialect(sqlalchemy_hive.HiveDialect):
     info_rows_delimiter = ('# Detailed Table Information', '', '')
     partition_columns_names = ['# Partition Information', '# Partitioning']
 
-    # def _get_table_columns(self, connection, table_name, schema, extended=False):
-    #     full_table = table_name
-    #     if schema:
-    #         full_table = schema + '.' + table_name
-    #     # TODO using TGetColumnsReq hangs after sending TFetchResultsReq.
-    #     # Using DESCRIBE works but is uglier.
-    #     try:
-    #         # This needs the table name to be unescaped (no backticks).
-    #         extended = " FORMATTED" if extended else ""
-    #         rows = connection.execute('DESCRIBE{} {}'.format(extended, full_table)).fetchall()
-    #     except exc.OperationalError as e:
-    #         # Does the table exist?
-    #         regex_fmt = r'TExecuteStatementResp.*NoSuchTableException.*Table or view \'{}\'' \
-    #                     r' not found'
-    #         regex = regex_fmt.format(re.escape(table_name))
-    #         if re.search(regex, e.args[0]):
-    #             raise exc.NoSuchTableError(full_table)
-    #         elif schema:
-    #             schema_regex_fmt = r'TExecuteStatementResp.*NoSuchDatabaseException.*Database ' \
-    #                                r'\'{}\' not found'
-    #             schema_regex = schema_regex_fmt.format(re.escape(schema))
-    #             if re.search(schema_regex, e.args[0]):
-    #                 raise exc.NoSuchTableError(full_table)
-    #         else:
-    #             # When a hive-only column exists in a table
-    #             hive_regex_fmt = r'org.apache.spark.SparkException: Cannot recognize hive type ' \
-    #                              r'string'
-    #             if re.search(hive_regex_fmt, e.args[0]):
-    #                 raise exc.UnreflectableTableError
-    #             else:
-    #                 raise
-    #     else:
-    #         return rows
+    def _get_table_columns(self, connection, table_name, schema, extended=False):
+        full_table = table_name
+        if schema:
+            full_table = schema + '.' + table_name
+        # TODO using TGetColumnsReq hangs after sending TFetchResultsReq.
+        # Using DESCRIBE works but is uglier.
+        try:
+            extended = " FORMATTED" if extended else ""
+            rows = connection.execute('DESCRIBE{} {}'.format(extended, full_table)).fetchall()
+        except exc.OperationalError as e:
+            regex_fmt = r'TExecuteStatementResp.*AnalysisException.*Table or view not found:.*{}'
+            hive_regex_fmt = r'org.apache.spark.SparkException: Cannot recognize hive type ' \
+                             r'string'
+            if re.search(regex_fmt.format(re.escape(table_name)), e.args[0]):
+                raise exc.NoSuchTableError(full_table)
+            elif re.search(hive_regex_fmt, e.args[0]):
+                raise exc.UnreflectableTableError
+            else:
+                raise
+        else:
+            return rows
 
     def get_table_names(self, connection, schema=None, **kw):
         # we SHOW TABLES will show tables and views, SHOW VIEWS only views, if it is needed we could potentially
